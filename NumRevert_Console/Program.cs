b19c1ce40187e9120b055a4d3c40f494ab0c68cb
linkomizin <linkomizin@gmail.com>
2024-04-26 10:59:28 +0300

    implement algorithm

--- NumRevert_Console/Program.cs
+++ NumRevert_Console/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security;
+
 Console.WriteLine("Hello, World!");
+
+
+int numToRevert = -12321;
+
+bool resultPalindrome = ItsPalindrome(numToRevert);
+
+Console.WriteLine($"Number {numToRevert} is palindrome = {resultPalindrome}");
+
+static bool ItsPalindrome(int number)
+{
+    if (number < 0)
+    {
+        return false;
+    }
+
+    int temp = number;
+    int currentNumber = 0;
+    int stage;
+
+    while (temp > 0)
+    {
+        stage = temp % 10;
+
+        currentNumber = currentNumber * 10 + stage;
+
+        temp = temp / 10;
+
+    }
+
+    return number.Equals(currentNumber);
+}
